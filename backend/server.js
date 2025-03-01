5f6a0a70b71b8aac5d9a18481665990460d6d91f
SriHariniPro <143264370+SriHariniPro@users.noreply.github.com>
2025-03-01 08:22:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 // CORS Configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: '*',  // Allow all origins in development
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
